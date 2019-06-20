0c8c70588e7a52c518e14fa04d3e2037b6b37777
Rune Andersen <rune@putty.dk>
2019-06-20 15:46:47 +0200

    worked on issue 2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$data = ['key' => 'value'];
+$data = ['version' => 'ISSUE-2'];
 header('Content-Type: application/json');
  header("Access-Control-Allow-Origin: *");
 echo json_encode($data);
